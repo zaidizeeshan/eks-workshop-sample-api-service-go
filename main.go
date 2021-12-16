a5f887a55427c074d5da3284ffefc8c24410046f
Zeeshan Zaidi <zeeshanzaidi4u@gmail.com>
2021-12-16 12:31:06 +0530

    update mesage text again

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello Zeeshan Zaidi"}
+		res := &response{Message: "Hello India"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
